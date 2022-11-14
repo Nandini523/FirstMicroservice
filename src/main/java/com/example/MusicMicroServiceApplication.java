f60386e2d2f34eeaf14e1ef69560e065bee6fa2e
Nandini523 <nandini0698@gmail.com>
2022-11-14 12:00:13 +0530

    first commit

--- src/main/java/com/example/MusicMicroServiceApplication.java
+++ src/main/java/com/example/MusicMicroServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicMicroServiceApplication {
 
+
+	//first commit
+
 	public static void main(String[] args) {
 		SpringApplication.run(MusicMicroServiceApplication.class, args);
 	}
